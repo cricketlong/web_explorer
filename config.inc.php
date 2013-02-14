96f7597346ed90ec92f530d5a6b9be72efc327ea
cricketlong <cricketlong@gmail.com>
2013-02-14 22:18:55 +0100

    add sample database

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 //root directory
-define("ROOT_DIR", getcwd()."/www");
+define("ROOT_DIR", getcwd()."/www");	//all the user directories are in the directory "www"
 define("COOKIE_EXPIRE_TIME", 300);	//in second
 
 ?>
